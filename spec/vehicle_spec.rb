c21cb629585c245cbf0b27cf4377d1a5a3d01e0a
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 09:35:15 -0600

    test: add test for add_passenger to vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -42,4 +42,21 @@ RSpec.describe Vehicle do
       expect(vehicle.speeding?).to be true
     end
   end
+
+  describe "#add_passenger" do
+    it "adds a passenger to the vehicle" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(vehicle.passengers).to be_empty
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    end
+  end
 end
