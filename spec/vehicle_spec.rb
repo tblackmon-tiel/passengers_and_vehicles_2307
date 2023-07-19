4b2b6eeb97625ce3d26f4527d44ccb96359b6743
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 09:28:28 -0600

    feat: add year, make, model attributes to vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe Vehicle do
 
       expect(vehicle).to be_a Vehicle
     end
+
+    it "has a year, make and model" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.year).to eq("2001")
+      expect(vehicle.make).to eq("Honda")
+      expect(vehicle.model).to eq("Civic")
+    end
   end
 end
