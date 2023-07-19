14b9f1e364d6bfddbfa36e2d7b90d8823e21e972
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 09:57:26 -0600

    test: add test for passengers to park

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -40,4 +40,30 @@ RSpec.describe Park do
       expect(park.vehicles).to eq([fusion, yukon])
     end
   end
+
+  describe "#passengers" do
+    it "returns a list of all passengers in all vehicles that entered the park" do
+      park = Park.new("Mesa Verde", 10)
+      fusion = Vehicle.new("2020", "Ford", "Fusion")
+      yukon = Vehicle.new("2005", "GMC", "Yukon")
+      tyler = Person.new("Tyler", 27)
+      kiwi = Person.new("Kiwi", 5)
+      chicken = Person.new("Chicken", 6)
+      bill = Person.new("Bill", 65)
+      josh = Person.new("Josh", 34)
+
+      expect(park.passengers).to be_empty
+
+      fusion.add_passenger(tyler)
+      fusion.add_passenger(kiwi)
+      fusion.add_passenger(chicken)
+      yukon.add_passenger(bill)
+      yukon.add_passenger(josh)
+
+      park.add_vehicle(fusion)
+      park.add_vehicle(yukon)
+
+      expect(park.passengers).to eq([tyler, kiwi, chicken, bill, josh])
+    end
+  end
 end
