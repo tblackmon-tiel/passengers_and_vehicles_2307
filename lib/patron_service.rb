17deb49a22ec20e60ce118da834d7ba440189d19
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 10:26:47 -0600

    feat: add all_attendees to patronservice

--- lib/patron_service.rb
+++ lib/patron_service.rb
@@ -8,4 +8,10 @@ class PatronService
   def add_park(park)
     @parks << park
   end
+
+  def all_attendees
+    @parks.map do |park|
+      park.passengers.map { |passenger| passenger.name }
+    end.flatten
+  end
 end
