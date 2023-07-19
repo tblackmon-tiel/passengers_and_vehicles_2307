cd559bcd4523b737faf613752030d977448ebf45
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 09:22:41 -0600

    test: add tests for drive and driver? to passenger

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -30,4 +30,29 @@ RSpec.describe Passenger do
       expect(taylor.adult?).to be false
     end
   end
+
+  describe "#driver?" do
+    it "returns true if the passenger is currently the driver, otherwise false" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.driver?).to be false
+      charlie.driver
+
+      expect(charlie.driver?).to be true
+      expect(taylor.driver?).to be false
+    end
+  end
+  
+  describe "#drive" do
+    it "sets the passenger as the driver" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    
+      expect(charlie.driver?).to be false
+      charlie.driver
+    
+      expect(charlie.driver?).to be true
+    end
+  end
 end
