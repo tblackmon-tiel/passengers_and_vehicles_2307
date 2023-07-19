4a9d3468e3185b3a70dae79ad14ab57dfd5ef53d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-19 09:50:25 -0600

    feat: add add_vehicle to park

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,15 @@
 class Park
-  attr_reader :name, :admission_price
+  attr_reader :name,
+              :admission_price,
+              :vehicles
 
   def initialize(name, admission_price)
     @name = name
     @admission_price = admission_price
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
   end
 end
